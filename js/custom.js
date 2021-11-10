683d8a9bf40d00230236c8e2c0a22e5e98fdca88
yemo0 <yemo0@users.noreply.github.com>
2021-11-10 16:34:31 +0000

    11-11-21 home page jump to posts yemo0/blog@ffddc32e79a02371c1593d64b8de7590fa2dacd3

--- js/custom.js
+++ js/custom.js
@@ -8,12 +8,11 @@
 // }
 
 
-// window.onload = function() {
-//     var s = document.getElementsByClassName("code-header")
-//     for(var i =0; i < s.length; i++) {
-//         s[i].style.background = "black"
-//     }
-// }
+window.onload = function() {
+   if (window.location.pathname == "/") {
+       window.location.replace("/posts/")
+   }
+}
 
 // document.addEventListener("DOMContentLoaded", function() {
 //     var s = document.getElementsByClassName("code-header")
--- tools/index.md
+++ tools/index.md
@@ -5,7 +5,8 @@
   - [imgURL图床](https://imgurl.org/)
 - 工具
   - [在线流程图](https://app.diagrams.net/)
-
+- 博客
+  - [go blog](https://www.liwenzhou.com/)
 
 
 
